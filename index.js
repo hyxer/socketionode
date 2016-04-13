d25f0ae79373274f5403d084aa089ad31b4e6d62
hyxer <harith87@me.com>
2016-04-13 13:11:10 +0800

    add xbee
    modules

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var util = require('util');
+var SerialPort = require('serialport').SerialPort;
+var xbee_api = require('xbee-api');
+
+var C = xbee_api.constants;
+
+var xbeeAPI = new xbee_api.XBeeAPI({
+    api_mode: 1
+});
+
+var serialport = new SerialPort("/dev/ttyUSB0", {
+    baudrate: 9600,
+    parser: xbeeAPI.rawParser()
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -10,7 +24,40 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('mode', function(msg){
+    if(msg=='open')
+    	{xbeeSend();
+    	console.log('open door');}
+	else
+		{console.log('else');}
+  });
+  
 });
+
+function xbeeSend(){
+ serialport.on("open", function () {
+    var frame_obj = { 
+        type: 0x00, 
+        id: 0x01, 
+        destination64: "0000000000000001",
+        options: 0x00, 
+        data: "1" 
+    };  
+    
+    serialport.write(xbeeAPI.buildFrame(frame_obj));
+    console.log('Sent to serial port.');
+    serialport.close();
+ });
+}
+
+
+
+
+
+
+
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
